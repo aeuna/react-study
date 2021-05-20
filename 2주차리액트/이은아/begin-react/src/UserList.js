87edb3d1f59d535813e54bc388014672fa01461b
aeuna <4294psy@naver.com>
2021-05-21 04:46:18 +0900

    #2 1-14 배열에 항목 제거하기

--- 2주차리액트/이은아/begin-react/src/UserList.js
+++ 2주차리액트/이은아/begin-react/src/UserList.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-const User = ({ user }) => {
+const User = ({ user, onRemove }) => {
   return (
     <div>
       <b>{user.username}</b> <span>({user.email})</span>
+      {/* jsx 외부에서 정의되어진 event handler 나 callback로 parm를 pass할때 arrow function을 쓴다. */}
+      <button onClick={() => onRemove(user.id)}>삭제 </button>
     </div>
   );
 };
 
-const UserList = ({ users }) => {
+const UserList = ({ users, onRemove }) => {
   return (
     <div>
       <div>
         {users.map((user) => (
-          <User user={user} key={user.id} />
+          <User user={user} key={user.id} onRemove={onRemove} />
         ))}
       </div>
     </div>
