5e8ad5613adf804ed510cd68275096fc030aec97
huiji0315 <huiji0315@gmail.com>
2021-05-24 20:42:59 +0900

    #2 1-12 useRef

--- src/UserList.js
+++ src/UserList.js
@@ -1,34 +1,19 @@
 import React from 'react';
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
+function User({ user }) {
   return (
     <div>
-      <div>
-        <b>{users[0].username}</b> <span>({users[0].email})</span>
-      </div>
-      <div>
-        <b>{users[1].username}</b> <span>({users[1].email})</span>
-      </div>
-      <div>
-        <b>{users[2].username}</b> <span>({users[1].email})</span>
-      </div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
+function UserList({ users }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
     </div>
   );
 }
