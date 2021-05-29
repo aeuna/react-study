f22ff69cd94179c9d2df586c2432cdfbe8dcb9ec
kiie9017 <39763880+kelee98@users.noreply.github.com>
2021-05-30 04:05:34 +0900

    #9 login 버튼 firebase 연동 완료

--- 3주차-NextJS/next-sns/pages/login.js
+++ 3주차-NextJS/next-sns/pages/login.js
@@ -1,7 +1,44 @@
 import React from 'react';
-
+import firebase from './firebase';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 const login = () => {
-  return <div>로그인페이지</div>;
+  const loginfuntion=() => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+    .then(function (result){
+      console.log('result.credential.accessToken',result.credential.accessToken);
+      console.log('result.user',result.user);
+      // myStore.use = {
+      //   displayName: result.user.displayName,
+      //   photoURL:result.user.photoURL,
+      //   email:result.user.email,
+      //   uid:result.user.uid
+      // }
+    })
+    .catch(error =>{
+      alert('login error:' +error.message);
+      console.log(error);
+    });
+  
+  };
+
+  return (
+  <div>
+   
+        <Button variant="contained" color="primary" onClick={loginfuntion} justify="center">
+        구글 로그인
+    </Button>
+  
+    
+
+    
+  </div>
+  );
 };
 
 export default login;
+
+
+
