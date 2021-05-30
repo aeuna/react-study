c591a558133ccfe28fbe8c36be9566566370f686
aeuna <4294psy@naver.com>
2021-05-30 19:48:52 +0900

    #8 피드 컴포넌트 구현

---
+++ 3주차-NextJS/next-sns/components/Feed.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import Link from "next/link";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "./Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ChatIcon from "@material-ui/icons/Chat";
+import Comment from "./Comment";
+import { Paper, TextField, Grid } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+
+const useStyles = makeStyles(() => ({
+  feed: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  root: {
+    marginTop: "20px",
+    maxWidth: "800px",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  comment: {
+    width: "600px",
+    height: "75px",
+  },
+  commentItem: {
+    marginTop: "10px",
+  },
+  commentSend: {
+    display: "flex",
+    flexDirection: "row",
+  },
+}));
+
+export default function Feed({ comments, setComments }) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [hearted, setHearted] = React.useState(false);
+  const [inputs, setInputs] = React.useState({
+    comment: "",
+  });
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleHeartClick = () => {
+    setHearted(!hearted);
+  };
+
+  const handleTextChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const handleSendClick = () => {
+    const comment = {
+      id: comments.length + 1,
+      username: "aeuna",
+      comment: inputs.comment,
+    };
+    setComments([...comments, comment]);
+    setInputs({
+      comment: "",
+    });
+  };
+
+  return (
+    <div className={classes.feed}>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={<Avatar size={1} />}
+          title="aeuna"
+          subheader="May 14, 2020"
+        />
+        <CardMedia
+          className={classes.media}
+          image="/images/test.jpeg"
+          title="food"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.... (데이터 들어오면 몇자 이내로 자를 예정!)
+          </Typography>
+          <Link href="/">
+            <Typography variant="body1">더보기</Typography>
+          </Link>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites" onClick={handleHeartClick}>
+            {hearted ? <FavoriteIcon color="secondary" /> : <FavoriteIcon />}
+          </IconButton>
+          <IconButton
+            aria-label="comment"
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+          >
+            <ChatIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Paper style={{ padding: "20px 20px" }}>
+              {comments.map((comment) => (
+                <Comment key={comment.id} data={comment} />
+              ))}
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid item>
+                  <Avatar size={1} />
+                </Grid>
+                <Grid
+                  className={classes.commentItem}
+                  justifyContent="left"
+                  item
+                  xs
+                  zeroMinWidth
+                >
+                  <h4 style={{ margin: 0, textAlign: "left" }}>aeuna</h4>
+                  <div className={classes.commentSend}>
+                    <TextField
+                      name="comment"
+                      multiline
+                      placeholder="댓글을 입력해주세요..."
+                      rowsMax={3}
+                      className={classes.comment}
+                      onChange={handleTextChange}
+                      value={inputs.comment}
+                    />
+                    <IconButton aria-label="send" onClick={handleSendClick}>
+                      <SendIcon />
+                    </IconButton>
+                  </div>
+                </Grid>
+              </Grid>
+            </Paper>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </div>
+  );
+}
