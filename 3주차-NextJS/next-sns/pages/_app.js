7325766eebdbd4cb3a9b22dd7c6b2c708064279b
aeuna <4294psy@naver.com>
2021-05-30 23:33:32 +0900

    #8 _app에 공통 컴포넌트,스타일 적용

--- 3주차-NextJS/next-sns/pages/_app.js
+++ 3주차-NextJS/next-sns/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Header from "../components/Header";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div style={{ marginTop: "65px" }}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
